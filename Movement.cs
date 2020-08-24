9252803ad0e121932e4855e80e9b307da008f405
AnujSapkota <69561662+AnujSapkota@users.noreply.github.com>
2020-08-24 20:49:10 +0545

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
